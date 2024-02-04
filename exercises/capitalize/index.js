f8e8c127dd3f3d9e4ad8fedffef57af971c5e535
Martin Nuñez <martin.nunezz@hotmail.com>
2024-02-04 12:26:41 -0300

    capitalize feature

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,23 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-
-  return str.split(" ").map( str => {
-    const arrStr = str.split("")
-    arrStr[0] =  arrStr[0].toUpperCase()
-
-    return arrStr.join("")
-  }).join(" ")
-}
-
-// ALTERNATIVE SOLUTIONS
-
 // function capitalize(str) {
-//   return str.split(" ").map(word => {
-//     const firstLetter = word[0].toUpperCase()
-//     return  firstLetter + word.slice(1)
+
+//   return str.split(" ").map( str => {
+//     const arrStr = str.split("")
+//     arrStr[0] =  arrStr[0].toUpperCase()
+
+//     return arrStr.join("")
 //   }).join(" ")
 // }
+
+// // ALTERNATIVE SOLUTIONS
+
+// // function capitalize(str) {
+// //   return str.split(" ").map(word => {
+// //     const firstLetter = word[0].toUpperCase()
+// //     return  firstLetter + word.slice(1)
+// //   }).join(" ")
+// // }
+
+
+
+
+
+function capitalize(params) {
+    if (/^[a-z]/.test(params))  return params.charAt(0).toUpperCase() + params.slice(1);
+    
+    
+    return params;
+}
+
+console.log(capitalize('a short sentence'));
+console.log(capitalize('a lazy fox'));
+console.log(capitalize('Look, it is working!'));
 
 module.exports = capitalize;
