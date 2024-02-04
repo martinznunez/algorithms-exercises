7db9d1ac007184d4cc4bd03403fbd9fa8e9e2aa0
Martin Nuñez <martin.nunezz@hotmail.com>
2024-02-04 12:11:16 -0300

    binarySearch feature

--- exercises/binarySearch/index.js
+++ exercises/binarySearch/index.js
@@ -1,27 +1,51 @@
 
 
+// function binarySearch(id, array) {
+//   let min = 0;
+//   let max = array.length - 1;
+//   let index;
+//   let element;
+
+//   while (min <= max) {
+//     index = Math.floor((min + max) / 2);
+//     element = array[index];
+
+//     if (element.id < id) {
+//       min = index + 1;
+//     } else if (element.id > id) {
+//       max = index - 1;
+//     } else {
+//       return element;
+//     }
+//   }
+
+//   return false;
+// }
+
+
+
+
 function binarySearch(id, array) {
-  let min = 0;
-  let max = array.length - 1;
-  let index;
-  let element;
-
-  while (min <= max) {
-    index = Math.floor((min + max) / 2);
-    element = array[index];
-
-    if (element.id < id) {
-      min = index + 1;
-    } else if (element.id > id) {
-      max = index - 1;
-    } else {
-      return element;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].id === id) {
+            return  array[i];
+        }
     }
-  }
-
-  return false;
+    return false;
 }
 
+
+const userArray = [
+    { id: 1, name: 'Alice' },
+    { id: 3, name: 'Bob' },
+    { id: 5, name: 'Charlie' },
+    { id: 7, name: 'David' },
+    { id: 9, name: 'Emma' },
+    { id: 11, name: 'Frank' },
+   
+  ];
+
+console.log( binarySearch (3,userArray )  )
 
 
 module.exports = binarySearch;
